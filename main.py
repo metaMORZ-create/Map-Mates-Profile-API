2e88b1d47ac9189781233f4b0d9cdd5295c71870
moritz <baarsmoritz70@gmail.com>
2025-04-04 19:43:32 +0200

    Adding Location Endpoint

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from db import engine
 import models as tables
-from routers import users  # <--- importiere deine Router
+from routers import users, locations  # <--- importiere deine Router
 
 app = FastAPI()
 
@@ -9,4 +9,4 @@ tables.Base.metadata.create_all(bind=engine)
 
 # Routen registrieren
 app.include_router(users.router, prefix="/users", tags=["users"])
-#app.include_router(locations.router, prefix="/locations", tags=["locations"])
+app.include_router(locations.router, prefix="/locations", tags=["locations"])
